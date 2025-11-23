8a494f68200285712b396867fa07e70913040d1b
Stefan Reichart <sr1989@icloud.com>
2025-11-23 21:48:57 +0100

    Config und Utils gefixt

---
+++ js/config.js
@@ -0,0 +1,18 @@
+export const APP_VERSION = "1.0.0";
+
+// Definition der Parkplätze
+export const PARKING_SPOTS = [
+    { id: 'P1', label: 'Parkplatz 1 (Links)' },
+    { id: 'P2', label: 'Parkplatz 2 (Rechts)' }
+];
+
+// Standard-Parkdauer in Stunden (für "Jetzt Parken")
+export const DEFAULT_PARKING_DURATION = 4;
+
+// Parteien im Haus (für das Dropdown beim Login)
+export const HOUSE_PARTIES = [
+    "Partei EG",
+    "Partei 1.OG",
+    "Partei 2.OG",
+    "Admin"
+];
